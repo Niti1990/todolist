6b722248d2add86b22d78f21eea5f28ef29aab67
PaLanzi <palanzillotta@googlemail.com>
2022-01-22 16:35:42 +0100

    pulled data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/Header/Header.css';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import List from './components/Main/Main';
 import Footer from './components/Footer/Footer';
-
+import Form from './components/Form/Form';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
-       
-        
-       
-        <Header/>
-        <Main/>
-       
+        <Form/>
+        <List/>
       </header>
+      
       <Footer/>
     </div>
   );
