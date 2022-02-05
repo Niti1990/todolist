c54fdd72a99f49fc0809aea80b743bfe0a74036d
PaLanzi <palanzillotta@googlemail.com>
2022-02-05 14:10:27 +0100

    new changes into add new todo

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,46 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
 function Form({ addNewTodo }) {
   // intialization the state
-  const [userInput, setUserInput] = useState('')
+  const [userInput, setUserInput] = useState("");
 
   const handleChange = (event) => {
-    setUserInput(event.target.value)
-  }
+    setUserInput(event.target.value);
+  };
 
   //   prevent form
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const newTodo = {
       id: new Date(),
       title: userInput,
       done: false,
-    }
-    addNewTodo(newTodo)
+    };
+    addNewTodo(newTodo);
 
-    setUserInput('')
-  }
+    setUserInput("");
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>Enter TODO</label>
         <input
-          type='text'
-          className='new_input'
-          placeholder='Enter to do'
+          type="text"
+          className="new_input"
+          placeholder="Enter to do"
           onChange={handleChange}
           value={userInput}
           required
         />
-        <button type='submit' className='button' onChange={handleChange}>
+        <button type="submit" className="button" onChange={handleChange}>
           Submit
         </button>
 
-        <button className='button' value='ClearAll'>
+        <button className="button" value="ClearAll">
           ClearAll
         </button>
       </form>
     </div>
-  )
+  );
 }
-export default Form
+export default Form;
